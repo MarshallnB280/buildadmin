7005d6d859c641ddb1cf45ebd72c0decf694e6eb
John Doe <244210065@qq.com>
2023-04-30 22:45:46 +0800

    积分变更弹出框

--- admin/web/src/views/backend/hexiao/admin/index.vue
+++ admin/web/src/views/backend/hexiao/admin/index.vue
@@ -18,13 +18,68 @@
         <!-- <MashangChannel v-bind:adminId="state.preview.adminId" /> -->
         <el-dialog v-model="state.preview.show" title="标题" :append-to-body="true" :center="true" class="ba-upload-preview">
             <div class="ba-upload-preview-scroll ba-scroll-style" style="height: 50vh">
-                <MashangChannel :adminId="state.preview.adminId" />
+                <el-scrollbar height="96vh">
+                    <el-form class="form-box" :model="items" label-width="120px" label-position="right">
+                        <!-- 以下四种输入框使用了 el-input -->
+                        <!-- input-attr 属性演示了如何添加 el-input 组件原有的属性 -->
+                        <FormItem
+                            label="昵称"
+                            type="string"
+                            v-model="items.nickname"
+                            :input-attr="{
+                                maxlength: 20,
+                                disabled: true, // 不允许输入的输入框
+                                clearable: true, // 事件以on开头，并使用大写驼峰的事件名称定义，不支持在此处单独传递$event（若需要请改用el-form-item）
+                            }"
+                            placeholder="昵称"
+                        />
+                        <FormItem
+                            label="ID"
+                            type="string"
+                            v-model="items.id"
+                            :input-attr="{
+                                maxlength: 20,
+                                disabled: true, // 不允许输入的输入框
+                                clearable: true, // 事件以on开头，并使用大写驼峰的事件名称定义，不支持在此处单独传递$event（若需要请改用el-form-item）
+                            }"
+                            placeholder="ID"
+                        />
+
+                        <FormItem
+                            label="当前积分"
+                            type="string"
+                            v-model.number="items.balance"
+                            :input-attr="{ clearable: true, size: 'large', maxlength: 10, disabled: true }"
+                        />
+                        <FormItem
+                            label="变更积分"
+                            type="number"
+                            v-model.number="items.number"
+                            :input-attr="{ clearable: true, size: 'large', maxlength: 8 }"
+                            placeholder="输入需要增加/扣除的积分"
+                        />
+                        <FormItem
+                            label="谷歌验证"
+                            type="string"
+                            v-model.number="items.googleKey"
+                            :input-attr="{ clearable: true, size: 'large', maxlength: 6 }"
+                            placeholder="输入6位谷歌秘钥"
+                        />
+                        <!-- <FormItem label="textarea" type="textarea" v-model="items.textarea" :input-attr="{ 'show-word-limit': true, rows: 3 }" /> -->
+
+                        <el-button v-blur @click="onSubmit(items)" type="primary" class="btn_"> + 添加积分 </el-button>
+                        <el-button v-blur @click="onSubmit(items)" type="danger" class="btn_"> - 扣除积分 </el-button>
+                    </el-form>
+                </el-scrollbar>
             </div>
         </el-dialog>
     </div>
 </template>
 
 <script setup lang="ts">
+import FormItem from '/@/components/formItem/index.vue'
+import createAxios from '/@/utils/axios'
+import { userUser } from '/@/api/controllerUrls'
 // import { ref, reactive, onMounted, watch, useSlots } from 'vue'
 import { ref, reactive, provide, onMounted, nextTick } from 'vue'
 import baTableClass from '/@/utils/baTable'
@@ -34,11 +89,19 @@ import { useI18n } from 'vue-i18n'
 import PopupForm from './popupForm.vue'
 import Table from '/@/components/table/index.vue'
 import TableHeader from '/@/components/table/header/index.vue'
-import MashangChannel from '../mashangChannel/index.vue'
+// import MashangChannel from '../mashangChannel/index.vue'
 import router from '/@/router/index'
 const { t } = useI18n()
 const tableRef = ref()
 const optButtons = defaultOptButtons(['edit', 'delete'])
+
+const items = reactive({
+    nickname: '',
+    id: '',
+    balance: 1,
+    googleKey: '',
+    number: '',
+})
 
 const state: {
     key: string
@@ -61,6 +124,33 @@ const state: {
         show_: true,
     },
 })
+
+// 积分变更-表单提交
+const onSubmit = (data: anyObj) => {
+    console.log(data)
+    return createAxios(
+        {
+            url: '/index.php/admin/auth.menu/index',
+            method: 'post',
+            data: data,
+        },
+        {
+            CancelDuplicateRequest: true, // 是否开启取消重复请求（连续的重复请求会被自动取消）
+            loading: true, // 是否开启loading层效果
+            showCodeMessage: true, // 是否开启code!=0时的信息直接提示（比如操作失败直接弹窗提示接口返回的消息，就不需要再写代码弹窗提示了）
+            showErrorMessage: true, // 是否开启接口错误信息直接提示，如接口404
+        },
+        {
+            text: '正在全力提交中...',
+        }
+    ).then((res) => {
+        // 请求回调-大部分情况不需要手动处理
+        console.log(res)
+        state.preview.show = false
+    })
+}
+
+// 然后就可以
 
 // 自定义一个新的按钮
 let newButton: OptButton[] = [
@@ -85,6 +175,46 @@ let newButton: OptButton[] = [
             // alert(1)
             console.log(row.id)
             router.push({ name: 'hexiao/mashangChannel', query: { adminId: row.id } })
+            // nextTick(() => {
+            //     state.preview.show = true
+            // })
+        },
+        // 按钮是否显示，请返回布尔值
+        display: (row: TableRow, field: TableColumn) => {
+            return true
+        },
+        // 按钮是否禁用，请返回布尔值
+        disabled: (row: TableRow, field: TableColumn) => {
+            return false
+        },
+        // 自定义el-button属性
+        attr: {},
+    },
+    {
+        // 渲染方式:tipButton=带tip的按钮,confirmButton=带确认框的按钮,moveButton=移动按钮
+        render: 'tipButton',
+        // 按钮名称
+        name: 'info',
+        // 鼠标放置时的 title 提示
+        title: '积分变更',
+        // 直接在按钮内显示的文字，title 有值时可为空
+        text: '积分变更',
+        // 按钮类型，请参考 element plus 的按钮类型
+        type: 'primary',
+        // 按钮 icon
+        icon: '',
+        class: 'table-row-info',
+        // tipButton 禁用 tip
+        disabledTip: false,
+        // 自定义点击事件
+        click: (row: TableRow, field: TableColumn) => {
+            console.log(row)
+            console.log(row.id)
+            items.nickname = row.nickname
+            items.id = row.id
+            items.balance = row.balance
+            state.preview.show = true
+            // router.push({ name: 'hexiao/mashangChannel', query: { adminId: row.id } })
             // nextTick(() => {
             //     state.preview.show = true
             // })
@@ -204,9 +334,21 @@ onMounted(() => {
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Disable from '/@/layouts/frontend/container/disable.vue'
 export default defineComponent({
     name: 'hexiao/admin',
 })
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.form-box {
+    width: 98%;
+    height: 50vh;
+    margin: 40px auto;
+}
+.btn_ {
+    float: right;
+    width: 120px;
+    margin-left: 20px;
+}
+</style>
